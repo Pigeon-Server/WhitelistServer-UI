8c48f49fd371520cc1f4ae0afe682f22d0f869d5
fsj <47785500+hhhfsj@users.noreply.github.com>
2022-08-02 15:53:57 +0800

    第一次提交~

---
+++ resource/javascript/Test_Page.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+
+    document.oncontextmenu=new Function("event.returnValue=false");
+    document.onselectstart=new Function("event.returnValue=false");
+
+    // Vue部分
+    Vue.config.productionTip = false
+    let vm = new Vue({
+        el: "#Test_Box",
+        data:{
+            // 存储试题
+            get_data: {}
+        },
+        created(){
+            this.get_Question_data()
+        },
+        methods: {
+            // 从接口获取试题
+            get_Question_data(){
+                axios
+                    .get("https://pigeon-server.cn:9000/api/test")
+                    .catch(function (error){
+                        console.log("[获取试题时发生严重错误]",error.message)
+                        alert("获取试题时发生严重错误")
+                        location.reload();
+                    })
+                    .then(function (data){
+                        vm.get_data = data.data
+                        document.querySelector(".test_questions").style.display = "block"
+                        document.getElementsByClassName("loading_cover")[0].style.display = "none";
+                    })
+            }
+        }
+    })
+}
